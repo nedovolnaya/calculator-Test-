bd0fee6f9ab33c6d3311f00b3f1c7f03b1956166
Unteryana <ishtar_v@bk.ru>
2023-07-19 18:19:59 +0300

    init commit

---
+++ src/main/java/skypro/calculatorTest/CalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package skypro.calculatorTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorTestApplication.class, args);
+	}
+
+}
